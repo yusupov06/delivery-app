004676754179f74edd9e1c50d06ae4663d87aeb9
yusupov06 <yusupovforwin@gmail.com>
2023-04-01 16:40:06 +0500

    second major changes

---
+++ src/test/java/uz/md/shopapp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.md.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
